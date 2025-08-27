0d2bf05214e30222807c2253d111f94a0992cee0
H82M19\Student <Student>
2025-08-27 08:02:04 +0700

    Lab 2. A. 2- Function Promise

---
+++ BaiTapTuan02_27082025/dist/a_2_promise.js
@@ -0,0 +1,9 @@
+"use strict";
+function getNumber() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(10);
+        }, 1000);
+    });
+}
+getNumber().then(console.log);
