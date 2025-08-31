64cfefbeb5bed0c2e3c1e5ab56ce1d0c11ca75da
hongnhientran.doylely@gmail.com <hongnhientran.doylely@gmail.com>
2025-08-31 14:37:37 +0700

    Lab 2. A. 7 - Promise Race

---
+++ BaiTapTuan02_27082025/dist/a_7_promise.js
@@ -0,0 +1,14 @@
+"use strict";
+const promise1 = new Promise((resolve) => {
+    setTimeout(() => resolve("Promise1 - 3s"), 3000);
+});
+const promise2 = new Promise((resolve) => {
+    setTimeout(() => resolve("Promise2 - 1s"), 1000);
+});
+const promise3 = new Promise((resolve) => {
+    setTimeout(() => resolve("Promise3 - 2s"), 2000);
+});
+Promise.race([promise1, promise2, promise3])
+    .then((value) => {
+    console.log("First: ", value);
+});
