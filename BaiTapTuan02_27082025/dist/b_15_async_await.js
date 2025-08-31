b93b3ab1aa20bac2c753d58c76fefc5f75c65f69
hongnhientran.doylely@gmail.com <hongnhientran.doylely@gmail.com>
2025-08-31 21:08:59 +0700

    Lab 2. B. 15 - multi async

---
+++ BaiTapTuan02_27082025/dist/b_15_async_await.js
@@ -0,0 +1,27 @@
+"use strict";
+function task1() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Task 1 - 1s");
+        }, 1000);
+    });
+}
+function task2() {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Task 2 - 2s");
+        }, 2000);
+    });
+}
+async function run() {
+    try {
+        const res1 = await task2();
+        console.log(res1);
+        const res2 = await task1();
+        console.log(res2);
+    }
+    catch (err) {
+        console.error(err);
+    }
+}
+run();
